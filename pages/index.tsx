8ef30e1857b70f07009afe2714af0c621f78f34c
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-09-17 18:22:46 +0530

    feat: logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import { NextPageWithLayout } from './page';
 import PrimaryLayout from '../components/templates/layouts/primary/PrimaryLayout';
 import SearchTemplate from '../components/templates/utility/search/SearchTemplate';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
   return (
-    <section>
+    <section className='flex flex-col items-center gap-y-5 mt-10 sm:mt-30'>
       {/* logo */}
+      <Image src="/GLogo.png" alt="logo" width={300} height={100} />
       <SearchTemplate sampleTextProp={'Search Google'} />
       {/* Toggle */}
+      <p>
+        Google offered in:{' '}
+        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
+          <a className='underline text-blue-600'> Françias</a>
+        </Link>
+      </p>
     </section>
   );
 };
