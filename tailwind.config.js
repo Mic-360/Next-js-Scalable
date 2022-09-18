a855f027fdf0d556a87e70ab3b8a31780f02ddc8
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-09-18 18:22:30 +0530

    feat: tailwind themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     // Ensure these match with .storybook/preview.js
@@ -12,6 +11,20 @@ module.exports = {
       md: '900px',
       lg: '1200px',
       xl: '1536px',
+    },
+    fontFamily: {
+      sans: ['Arial', 'sans-serif'],
+      serif: ['Garamond', 'serif'],
+    },
+    extend: {
+      colors: {
+        blue: {
+          500: '#1a73e8',
+        },
+      },
+      spacing: {
+        128: '32rem',
+      },
     },
   },
   plugins: [],
